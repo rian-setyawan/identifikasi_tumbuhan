778ef7889ab944ab1d3d41b9277e25046e4e65a0
rian-setyawan <riansetyawan72@gmail.com>
2019-05-25 23:15:25 +0700

    FILE 1

---
+++ menu.m
@@ -0,0 +1,119 @@
+function varargout = menu(varargin)
+% MENU MATLAB code for menu.fig
+%      MENU, by itself, creates a new MENU or raises the existing
+%      singleton*.
+%
+%      H = MENU returns the handle to a new MENU or the handle to
+%      the existing singleton*.
+%
+%      MENU('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MENU.M with the given input arguments.
+%
+%      MENU('Property','Value',...) creates a new MENU or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before menu_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to menu_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help menu
+
+% Last Modified by GUIDE v2.5 16-May-2019 22:19:18
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @menu_OpeningFcn, ...
+                   'gui_OutputFcn',  @menu_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before menu is made visible.
+function menu_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to menu (see VARARGIN)
+
+% Choose default command line output for menu
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+movegui(hObject,'center');
+hback = axes('units','normalized','position',[0 0 1 1]);
+uistack(hback,'bottom'); % menciptakan axes untuk tempat menampilkan gambar
+% menampilkan background
+[back map]=imread('home.jpg');
+image(back)
+colormap(map)
+background=imread('home.jpg');
+% set(handles.pan1,'CData',background);
+% handlevisibility off agar axes tidak terlihat
+% dan gambar background saja yang muncul.
+set(hback,'handlevisibility','off','visible','off')
+% Update handles structure
+guidata(hObject, handles);
+% UIWAIT makes menu wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = menu_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+identifikasi_tumbuhan
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+close(menu);
+about
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+pilihan = questdlg('Apakah anda ingin menutup program?', ...
+	'Menutup Program', ...
+	'Ya','Tidak','Tidak');
+% Handle response
+switch pilihan
+    case 'Ya'
+        close(menu);
+    case 'Tidak'
+        return;
+end
